5a5f2a0c1a3bded037e72248d52d696469eae53d
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2024-01-14 05:31:27 -0500

    add runqemu script

---
+++ runqemu.sh
@@ -0,0 +1,4 @@
+./qemu-system-aarch64 -M virt -m 1G -cpu cortex-a76 -nographic -serial mon:stdio \
+	-kernel "/Users/zhuowei/Documents/winprogress/donair/linux64k/image-patched" \
+	-append "root=/dev/vda1 rootflags=\"iocharset=cp437\" kpti=0 nokaslr=1 init=/init" \
+	-drive file=fat:/Users/zhuowei/Documents/winprogress/donair/rootdir4,format=raw,readonly=on,media=disk
