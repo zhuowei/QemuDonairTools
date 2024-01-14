c70047d532b6b02445b393e6b288cabf7af98cf6
Zhuowei Zhang <zhuoweizhang@yahoo.com>
2024-01-14 14:32:06 -0500

    test fork in the hello world program

--- linux_dynamic_hello/hello.c
+++ linux_dynamic_hello/hello.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+static int pid;
+
+extern char __executable_start;
 
 int main() {
+  printf("Starting up: base=%p &pid=%p\n", &__executable_start, &pid);
+  int result = fork();
+  pid = getpid();
   for (int i = 0; i < 10; i++) {
-    printf("Hello world :D %d\n", i);
+    printf("[pid=%d] Hello world :D %d\n", pid, i);
+    usleep(1000);
+  }
+  if (result) {
+    wait(NULL);
   }
   return 42;
 }
